c9c94d792eac4845f1e9e42e93fd36aa53dd073e
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2025-03-17 10:29:48 -0700

    lower resolution thumbnails

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,5 +1,4 @@
-for file in images/*.{jpg,png}; do
+for file in images/*.{jpg,png,gif}; do
     [ ! -f "tn/$file" ] && magick "$file"  -thumbnail 250x250 "tn/$file"
 done
 
-cp images/*.gif tn/images/
